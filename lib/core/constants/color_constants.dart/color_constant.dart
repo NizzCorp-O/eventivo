4ddb5caba1f3d0fd9c82984ef3aebb8a5103b1a2
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-06 19:04:43 +0530

    added new constannt colors

--- lib/core/constants/color_constants.dart/color_constant.dart
+++ lib/core/constants/color_constants.dart/color_constant.dart
@@ -5,11 +5,11 @@ class ColorConstant {
   static const Color CircularProgressIndicatorBG = Colors.blueAccent;
   static const Color MainBlack = Color(0xFF000000);
   static const Color MainWhite = Color(0xFFFFFFFF);
-
   static const Color GradientColor1 = Color(0xFFD4AF37);
   static const Color GradientColor2 = Color(0xFFEAB308);
   static const Color InputBorder = Color(0xFFE5E7EB);
   static const Color InputText = Color(0xFFADAEBC);
   static const Color Subtittle = Color(0xFF4B5563);
-  
+  static const Color textfieldBG = Color(0xFFF9FAFB);
+  static const Color PrimaryBlue = Color(0xFF1A237E);
 }
