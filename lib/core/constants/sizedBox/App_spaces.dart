8d58af3b4220ff503604b12aa1bc223ffb78d727
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-09 20:04:57 +0530

    create a new class :app spaces

---
+++ lib/core/constants/sizedBox/App_spaces.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AppSpaces {
+  static const SizedBox height21 = SizedBox(height: 21);
+  static const SizedBox height7 = SizedBox(height: 7);
+   static const SizedBox height16 = SizedBox(height: 16);
+     static const SizedBox width10 = SizedBox(width: 10,);
+}
