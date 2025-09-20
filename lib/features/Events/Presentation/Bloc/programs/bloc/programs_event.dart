37d42640c605c618704bde2d64ade23ce34759f7
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-20 17:21:17 +0530

    update and delete functionality is completed

--- lib/features/Events/Presentation/Bloc/programs/bloc/programs_event.dart
+++ lib/features/Events/Presentation/Bloc/programs/bloc/programs_event.dart
@@ -1,14 +1,42 @@
 part of 'programs_bloc.dart';
 
- class ProgramsEvent {}
- class AddProgramEvent extends ProgramsEvent {
-  final  ProgramModel programmodel;
+class ProgramsEvent {}
+
+class AddProgramEvent extends ProgramsEvent {
+  final ProgramModel programmodel;
   final String ID;
   AddProgramEvent({required this.programmodel, required this.ID});
 }
+
 class GetProgramsEvent extends ProgramsEvent {
   final String Id;
   // List<ProgramModel>programs;
 
   GetProgramsEvent(this.Id);
 }
+
+class UpdateProgram extends ProgramsEvent {
+  final ProgramModel progrmmodel;
+  final String ID;
+
+  UpdateProgram(this.ID, {required this.progrmmodel});
+}
+
+class DeleteProgram extends ProgramsEvent {
+final String programid;
+  final String ID;
+
+  DeleteProgram({required this.programid, required this.ID});
+}
+class ReorderEvents extends ProgramsEvent {
+  final String ID;       // Event ID for Firestore
+  final int oldIndex;
+  final int newIndex;
+
+  ReorderEvents({
+    required this.ID,
+    required this.oldIndex,
+    required this.newIndex,
+  });
+}
+
