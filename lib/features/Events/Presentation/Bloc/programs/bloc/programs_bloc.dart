ec2bab2ef7f9362c96f62b35e3ea93f8f4cc6c8b
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-20 17:20:25 +0530

    updated program bloc

--- lib/features/Events/Presentation/Bloc/programs/bloc/programs_bloc.dart
+++ lib/features/Events/Presentation/Bloc/programs/bloc/programs_bloc.dart
@@ -27,5 +27,44 @@ class ProgramsBloc extends Bloc<ProgramsEvent, ProgramsState> {
         emit(ProgramError(message: "No progorams currently availble"));
       }
     });
+    on<UpdateProgram>((event, emit) async {
+      emit(Programloading());
+      try {
+        await programrepo.updateProgram(event.progrmmodel, event.ID);
+        final programs = await programrepo.getProgramsOnce(event.ID);
+        emit(ProgramsLoaded(programs));
+      } catch (e) {
+        emit(ProgramError(message: "update failed"));
+      }
+    });
+
+    on<DeleteProgram>((event, emit) async {
+      emit(Programloading());
+      try {
+        await programrepo.deleteProgram(event.programid, event.ID);
+        final programs = await programrepo.getProgramsOnce(event.ID);
+        emit(ProgramsLoaded(programs));
+      } catch (e) {
+        emit(ProgramError(message: "does not delete this program"));
+      }
+    });
+on<ReorderEvents>((event, emit) async {
+  if (state is ProgramsLoaded) {
+    final currentState = state as ProgramsLoaded;
+    final updatedList = List<ProgramModel>.from(currentState.programs);
+
+    int newIndex = event.newIndex;
+    if (newIndex > event.oldIndex) newIndex -= 1;
+
+    final item = updatedList.removeAt(event.oldIndex);
+    updatedList.insert(newIndex, item);
+
+    // 🔥 Update Firestore order
+    await programrepo.updateProgramsOrder(event.ID, updatedList);
+
+    emit(ProgramsLoaded(updatedList));
+  }
+});
+
   }
 }
