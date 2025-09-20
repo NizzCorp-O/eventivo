4d8f4a50a77cd4d0c2be5874c642797522522092
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-20 17:22:07 +0530

    updated profile screen

--- lib/features/Events/Presentation/screens/participant_dashboard.dart/profile_screen.dart
+++ lib/features/Events/Presentation/screens/participant_dashboard.dart/profile_screen.dart
@@ -1,17 +1,26 @@
 import 'package:eventivo/core/constants/color_constants.dart/color_constant.dart';
 import 'package:eventivo/core/utils%20/fonts.dart';
+import 'package:eventivo/features/Events/Presentation/Bloc/event_bloc.dart';
 import 'package:eventivo/features/Events/Presentation/screens/admin_dashbord.dart/event_creation_screen.dart';
+import 'package:eventivo/features/Events/Presentation/screens/admin_dashbord.dart/manage_event.dart';
 import 'package:eventivo/features/Events/Presentation/widgets/my_events.dart';
 import 'package:eventivo/features/Events/Presentation/widgets/upcoming_events.dart';
 import 'package:eventivo/features/auth/presentation/pages/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> images = [
+      "https://images.pexels.com/photos/6476783/pexels-photo-6476783.jpeg",
+      "https://images.pexels.com/photos/30319116/pexels-photo-30319116.jpeg",
+      "https://images.pexels.com/photos/7652058/pexels-photo-7652058.jpeg",
+      "https://images.pexels.com/photos/8067828/pexels-photo-8067828.jpeg",
+    ];
     return SafeArea(
       child: Scaffold(
         backgroundColor: ColorConstant.MainWhite,
@@ -166,16 +175,55 @@ class ProfileScreen extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(12),
-                          color: ColorConstant.PrimaryBlue
+                          color: ColorConstant.PrimaryBlue,
                         ),
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 16),
-                ////////////// MY EVENT ///////////////////
-                ///////////// MY EVENT ///////////////////
-                ...List.generate(3, (index) => My_Events()),
+                BlocConsumer<EventBloc, EventState>(
+                  listener: (context, state) {
+                    // TODO: implement listener
+                  },
+                  builder: (context, state) {
+                    if (state is EventFetched) {
+                      return Column(
+                        children:
+                            ////////////// MY EVENT ///////////////////
+                            ///////////// MY EVENT ///////////////////
+                            List.generate(
+                              state.Events.length,
+                              (index) => My_Events(
+                                title: state.Events[index].name,
+                                URL: state.Events[index].imageUrls[0],
+                                date: state.Events[index].date,
+                                time: state.Events[index].starttime,
+                                onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ManageEvent(
+                                      eventId: state.Events[index].id,
+                                      itemcount:
+                                          state.Events[index].imageUrls.length,
+                                      imageUrls: state.Events[index].imageUrls,
+                                      title: state.Events[index].name,
+                                      URL: state.Events[index].imageUrls[1],
+                                      AvailableSlote:
+                                          state.Events[index].availableSlot,
+                                      Entryfee: state.Events[index].entryFee,
+                                      location: state.Events[index].venue,
+                                      // imageUrl: images[index],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                      );
+                    }
+                    return Text("no event registered");
+                  },
+                ),
 
                 SizedBox(height: 20),
                 Center(
@@ -187,23 +235,33 @@ class ProfileScreen extends StatelessWidget {
                       );
                     },
                     //////// Logout Button //////
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 12,
-                        horizontal: 16,
-                      ),
-                      decoration: BoxDecoration(
-                        color: ColorConstant.GradientColor1,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Logout",
-                          style: TextStyle(
-                            fontFamily: CustomFontss.fontFamily,
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 16,
+                        ),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5,
+                              offset: Offset(-1, 0),
+                            ),
+                          ],
+                          color: ColorConstant.GradientColor1,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Logout",
+                            style: TextStyle(
+                              fontFamily: CustomFontss.fontFamily,
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
