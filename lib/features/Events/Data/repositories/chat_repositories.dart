c115bde101e44868cb63f7b754af79690e9144a1
shafeeque mohd <shafeequemohd711@gmail.com>
2025-09-25 12:14:58 +0530

    New repositories (chat)

--- lib/features/Events/Data/repositories/chat_repositories.dart
+++ lib/features/Events/Data/repositories/chat_repositories.dart
@@ -1,26 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eventivo/features/Events/Data/models/chat_models.dart';
 
-class ChatRepository {
-  Stream<List<ChatModels>> getMessages(String eventId) {
-    return FirebaseFirestore.instance
-        .collection("chats")
+class ChatService {
+  Future<void> sendMessage(ChatModel message, String eventId) async {
+    await FirebaseFirestore.instance
+        .collection("events")
         .doc(eventId)
-        .collection("messages")
-        .orderBy("time", descending: true)
-        .snapshots()
-        .map(
-          (snapshot) => snapshot.docs
-              .map((doc) => ChatModels.fromJson(doc.data()))
-              .toList(),
-        );
+        .collection("chats")
+        .add(message.toJson());
   }
 
-  Future<void> sendMessage(ChatModels message, String eventId) async {
-    await FirebaseFirestore.instance
-        .collection("chats")
+  Stream<List<ChatModel>> getMessages(String eventId) {
+    return FirebaseFirestore.instance
+        .collection("events")
         .doc(eventId)
-        .collection("messages")
-        .add(message.toJson());
+        .collection("chats")
+        .orderBy("timestamp", descending: true)
+        .snapshots()
+        .map((snapshot) =>
+            snapshot.docs.map((doc) => ChatModel.fromJson(doc.data())).toList());
   }
 }
